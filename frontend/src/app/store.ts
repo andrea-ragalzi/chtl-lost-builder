e4898ef480bbe089aafcd516bc7d0b9d4b5fda8c
Andrea Ragalzi <andrea.ragalzi.code@gmail.com>
2025-10-02 21:37:25 +0200

    added redux-persist

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,44 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import characterReducer from '../shared/stores';
-import authReducer from '../features/auth/stores/authSlice'; // Import the auth reducer
+import authReducer from '../features/auth/stores/authSlice';
+
+
+const characterPersistConfig = {
+  key: 'character',
+  version: 1,
+  storage
+};
+
+const persistedCharacterReducer = persistReducer(characterPersistConfig, characterReducer);
+
 
 export const store = configureStore({
   reducer: {
-    character: characterReducer,
-    auth: authReducer
+    character: persistedCharacterReducer,
+    auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
